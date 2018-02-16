f934212a9ea23f771158c1d02dda19e2a97759ed
Cyril Vmd <cyril@cyrilwebdesign.com>
2018-02-16 21:29:19 +0100

    Clean code

--- src/Block.php
+++ src/Block.php
@@ -4,22 +4,22 @@ namespace Cyve\Blockchain;
 
 class Block
 {
-	public $timestamp;
-	public $payload;
-	public $hash;
-	public $previousHash;
-	public $nonce = 0;
-	
-	public function __construct($timestamp, $payload, $previousHash)
-	{
-		$this->timestamp = (int) $timestamp;
-		$this->payload = base64_encode($payload);
-		$this->previousHash = $previousHash;
-		$this->hash = $this->generateHash();
-	}
-	
-	public function generateHash()
-	{
-		return hash('sha256', $this->timestamp.'.'.$this->payload.'.'.$this->previousHash.'.'.$this->nonce);
-	}
+    public $timestamp;
+    public $payload;
+    public $hash;
+    public $previousHash;
+    public $nonce = 0;
+    
+    public function __construct($timestamp, $payload, $previousHash)
+    {
+        $this->timestamp = (int) $timestamp;
+        $this->payload = base64_encode($payload);
+        $this->previousHash = $previousHash;
+        $this->hash = $this->generateHash();
+    }
+    
+    public function generateHash()
+    {
+        return hash('sha256', $this->timestamp.'.'.$this->payload.'.'.$this->previousHash.'.'.$this->nonce);
+    }
 }
--- src/Blockchain.php
+++ src/Blockchain.php
@@ -4,54 +4,54 @@ namespace Cyve\Blockchain;
 
 class Blockchain
 {
-	const DIFFICULTY = 2;
-	
-	public $blocks = [];
-	
-	public function __construct()
-	{
-		$this->blocks[] = new Block(microtime(true), 'init', null);
-	}
-	
-	public function isValidFirstBlock(Block $block)
-	{
-		return base64_decode($block->payload) === 'init' && $block->previousHash === null && $block->generateHash() === $block->hash;
-	}
-	
-	public function isValidBlock(Block $block, Block $previous)
-	{
-		return $block->timestamp >= $previous->timestamp && $block->previousHash === $previous->hash && $block->generateHash() === $block->hash && $this->validateProofOfWork($block, self::DIFFICULTY);
-	}
-	
-	public function isValid()
-	{
-		if(!$this->isValidFirstBlock($this->blocks[0])) return false;
-		
-		for($i=1,$l=count($this->blocks); $i<$l; $i++){
-			if(!$this->isValidBlock($this->blocks[$i], $this->blocks[$i-1])) return false;
-		}
-		
-		return true;
-	}
-	
-	public function add(Block $block)
-	{
-		$this->blocks[] = $block;
-	}
-	
-	public function createNewBlock($payload)
-	{
-		$block = new Block(microtime(true), $payload, end($this->blocks)->hash);
-		
-		while(!$this->validateProofOfWork($block, self::DIFFICULTY)){
-			$block->nonce++;
-			$block->hash = $block->generateHash();
-		}
-		
-		return $block;
-	}
-	
-	public function validateProofOfWork(Block $block, int $difficulty){
-		return substr($block->hash, 0, $difficulty) === str_pad('', $difficulty, '0');
-	}
+    const DIFFICULTY = 2;
+    
+    public $blocks = [];
+    
+    public function __construct()
+    {
+        $this->blocks[] = new Block(microtime(true), 'init', null);
+    }
+    
+    public function isValidFirstBlock(Block $block)
+    {
+        return base64_decode($block->payload) === 'init' && $block->previousHash === null && $block->generateHash() === $block->hash;
+    }
+    
+    public function isValidBlock(Block $block, Block $previous)
+    {
+        return $block->timestamp >= $previous->timestamp && $block->previousHash === $previous->hash && $block->generateHash() === $block->hash && $this->validateProofOfWork($block, self::DIFFICULTY);
+    }
+    
+    public function isValid()
+    {
+        if(!$this->isValidFirstBlock($this->blocks[0])) return false;
+        
+        for($i=1,$l=count($this->blocks); $i<$l; $i++){
+            if(!$this->isValidBlock($this->blocks[$i], $this->blocks[$i-1])) return false;
+        }
+        
+        return true;
+    }
+    
+    public function add(Block $block)
+    {
+        $this->blocks[] = $block;
+    }
+    
+    public function createNewBlock($payload)
+    {
+        $block = new Block(microtime(true), $payload, end($this->blocks)->hash);
+        
+        while(!$this->validateProofOfWork($block, self::DIFFICULTY)){
+            $block->nonce++;
+            $block->hash = $block->generateHash();
+        }
+        
+        return $block;
+    }
+    
+    public function validateProofOfWork(Block $block, int $difficulty){
+        return substr($block->hash, 0, $difficulty) === str_pad('', $difficulty, '0');
+    }
 }
